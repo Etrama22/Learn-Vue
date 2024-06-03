4c7e48abef2b4392e3245f5f5af7622f7c4ef859
Etrama22 <muhazis73@gmail.com>
2024-06-04 01:30:52 +0700

    All Views

---
+++ src/views/ProfileView.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="profile">
+    <!-- <img alt="Vue logo" src="../assets/logo.png" /> -->
+    <!-- <h1>Ini adalah Logo</h1>
+    <img src="../assets/logo.png" alt="" /> -->
+    <div class="container">
+      <div class="row">
+        <div class="col-12">
+          <div class="d-flex align-items-center">
+            <div class="flex-shrink-0">
+              <img src="../assets/logo.png" alt="..." />
+            </div>
+            <div class="flex-grow-1 ms-3">
+              This is some content from a media component. You can replace this
+              with any content and adjust it as needed.
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import "bootstrap/dist/css/bootstrap.css";
+// @ is an alias to /src
+
+export default {
+  name: "HomeView",
+  components: {},
+};
+</script>
