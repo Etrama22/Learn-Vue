2477234dfb87469004b87f5fa0a6e879b898bc97
Etrama22 <muhazis73@gmail.com>
2024-06-04 15:56:09 +0700

    ProfileView

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -1,16 +1,18 @@
 <template>
   <div class="profile">
-    <!-- <img alt="Vue logo" src="../assets/logo.png" /> -->
-    <!-- <h1>Ini adalah Logo</h1>
-    <img src="../assets/logo.png" alt="" /> -->
     <div class="container">
       <div class="row">
-        <div class="col-12">
+        <div class="col-12 d-flex justify-content-center">
           <div class="d-flex align-items-center">
             <div class="flex-shrink-0">
-              <img src="../assets/logo.png" alt="..." />
+              <img
+                class="user-pic rounded-circle"
+                src="../assets/profile.jpeg"
+                alt="..."
+              />
             </div>
-            <div class="flex-grow-1 ms-3">
+            <div class="flex-grow-1 ms-3 text-start">
+              <h5 class="mt-0">Muhammad Azis Etrama</h5>
               This is some content from a media component. You can replace this
               with any content and adjust it as needed.
             </div>
@@ -30,3 +32,10 @@ export default {
   components: {},
 };
 </script>
+
+<style scoped>
+.user-pic {
+  width: 80px;
+  height: 80px;
+}
+</style>
